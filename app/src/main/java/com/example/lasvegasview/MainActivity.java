b6332896d5e7ac70ac48edb7426518591c7381a1
ThiruvengadaDixith <thiruvengadadixith@gmail.com>
2021-02-03 20:30:55 +0530

    first commit

--- app/src/main/java/com/example/lasvegasview/MainActivity.java
+++ app/src/main/java/com/example/lasvegasview/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.lasvegasview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+           // hide status bar and navigation bar at the bottom
+
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+                        );
+
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
+
     }
 }
