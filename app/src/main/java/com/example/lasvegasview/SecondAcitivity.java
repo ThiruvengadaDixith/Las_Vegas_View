b729eb5ac094a267ca3ce5cd3832971428634a09
ThiruvengadaDixith <thiruvengadadixith@gmail.com>
2021-02-04 19:14:37 +0530

    4/2/2021

---
+++ app/src/main/java/com/example/lasvegasview/SecondAcitivity.java
@@ -0,0 +1,14 @@
+package com.example.lasvegasview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_acitivity);
+    }
+}
