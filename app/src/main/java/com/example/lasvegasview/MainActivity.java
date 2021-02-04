8c3be9b50cdbc9bc9d758aeb50a388871bd328fa
ThiruvengadaDixith <thiruvengadadixith@gmail.com>
2021-02-04 13:53:18 +0530

    4/2/2021

--- app/src/main/java/com/example/lasvegasview/MainActivity.java
+++ app/src/main/java/com/example/lasvegasview/MainActivity.java
@@ -23,6 +23,11 @@ public class MainActivity extends AppCompatActivity {
 
         this.getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                |View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                |View.SYSTEM_UI_FLAG_FULLSCREEN
+                |View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                |View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                |View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         );
 
     }
